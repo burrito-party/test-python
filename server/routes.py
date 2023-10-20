3f1cff680951ae0b5b2fbe57427f99b370559f35
Dan Shanahan <leftrightleft@github.com>
2023-10-20 09:39:13 -0700

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -4,7 +4,7 @@ from flask import request, render_template, make_response
 from server.webapp import flaskapp, cursor
 from server.models import Book
 
-
+# This is where the API is
 @flaskapp.route('/')
 def index():
     name = request.args.get('name')
